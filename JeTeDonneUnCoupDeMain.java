301f66c046b616bf14771b062d983070b82a2a95
Pranxtor <pranxtor02@gmail.com>
2020-05-18 20:06:32 +0200

    oui

---
+++ JeTeDonneUnCoupDeMain.java
@@ -0,0 +1,10 @@
+
+public class JeTeDonneUnCoupDeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String main="Team 16";
+		System.out.println(main);
+	}
+
+}
--- OnDitUneMain.java
+++ OnDitUneMain.java
@@ -2,8 +2,15 @@
 public class OnDitUneMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 1;
+		int m = 4;
+		String s;
+		for(int i=1;i<n;i++) {
+			a = a * m;
+			m = m - 1;
+		}
+			
+		System.out.println(a);
 	}
 
 }
