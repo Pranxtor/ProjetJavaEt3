0c79602f9be11e9c7d83389fab31c001397f1d4e
tshanon <tshanon.lok@gmail.com>
2020-05-26 15:10:16 +0200

    liste de doc dans serie

--- src/Documents/Serie.java
+++ src/Documents/Serie.java
@@ -1,9 +1,11 @@
 package Documents;
 
+import java.util.ArrayList;
+
 public class Serie {
     private String titre;
     private Integer numero;
-
+    private ArrayList<Document> serie;
 
     // constructeur
     public Serie(String titre, Integer numero){
