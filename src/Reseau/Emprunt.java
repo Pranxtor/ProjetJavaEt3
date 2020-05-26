a82af6e1a7fb98855143a8a2996fc03f8e15a6f5
tshanon <tshanon.lok@gmail.com>
2020-05-26 15:19:39 +0200

    update : emprunt

--- src/Reseau/Emprunt.java
+++ src/Reseau/Emprunt.java
@@ -7,11 +7,16 @@ import java.util.ArrayList;
 public class Emprunt {
     private ArrayList<Document> listeEmprunt;
     private int nbLivreMax;
+    private Client client;
+    private Bibliotheque bibliotheque;
 
-    public Emprunt(int nombreLivreMax){
+    public Emprunt(int nombreLivreMax, Client client, Bibliotheque bibliotheque){
         this.nbLivreMax = nombreLivreMax;
         listeEmprunt = new ArrayList<>();
+        this.client = client;
+        this.bibliotheque = bibliotheque;
     }
+
 
 
 
