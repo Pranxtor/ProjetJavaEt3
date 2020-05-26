5ced8ee9a4a291e875bcbec14d94dac344ad03c7
Pranxtor <pranxtor02@gmail.com>
2020-05-26 15:47:31 +0200

    emprunt et retour pour bilbiotheque

--- src/Reseau/Bibliotheque.java
+++ src/Reseau/Bibliotheque.java
@@ -3,6 +3,7 @@ package Reseau;
 import Documents.Document;
 import Documents.Livre;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -12,6 +13,7 @@ public class Bibliotheque {
     private String adresse;
     private HashSet<Document> collection;
     private HashMap<String, Livre> searchISBN;
+    private HashSet<Client> clients;
 
     private static HashSet<Bibliotheque> reseauBibliotheque;
 
@@ -21,6 +23,62 @@ public class Bibliotheque {
         collection = new HashSet<>();
         searchISBN = new HashMap<>();
     }
+    
+    public boolean ajouterDocument(Document doc) {
+    	collection.add(doc);
+    	return true;
+    }
+    
+    public boolean emprunter() {
+    	boolean emprunt = Document.diminuerQuantite();
+    	return emprunt;
+    }
+    
+    public boolean rendre() {
+    	boolean retour = Document.augmenterQuantite();
+    	return retour;
+    }
+    
+    public ArrayList<Document> consulterToutDoc() {
+    	ArrayList<Document> docs= new ArrayList<Document>();
+    	for(Document doc : collection) {
+    		docs.add(doc);
+    	}
+    	return docs;
+    }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adresse == null) ? 0 : adresse.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bibliotheque other = (Bibliotheque) obj;
+		if (adresse == null) {
+			if (other.adresse != null)
+				return false;
+		} else if (!adresse.equals(other.adresse))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+    
+    
 
 
 
